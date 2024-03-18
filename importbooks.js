92196a4044d94053582a9250340f9e1bc6c52b61
Randi Gabbard <gabbardr@msu.edu>
2024-03-18 19:12:02 -0400

    changes

--- importbooks.js
+++ importbooks.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://teymsckkkueaiccnmbma.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRleW1zY2tra3VlYWljY25tYm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTg5NDUsImV4cCI6MjAyNjM3NDk0NX0.9b3sxKHSVSztgVP7t7FvX1WGZL-jubdRz_QwQ7JPCZQ'
+const supabaseKey = 
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRleW1zY2tra3VlYWljY25tYm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTg5NDUsImV4cCI6MjAyNjM3NDk0NX0.9b3sxKHSVSztgVP7t7FvX1WGZL-jubdRz_QwQ7JPCZQ'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 let { data: Five_Books, error } = await supabase
@@ -11,5 +12,3 @@ for (let My_Books of Five_Books) {
     let mybookTable = document.getElementById('Five_Books_Title');
     mybookTable.innerHTML += '<ul>${My_Books.Title}</ul>';
 }
-
-alert(1);
