b628d4f6c27248067216954e19f31520f1071c96
Randi Gabbard <gabbardr@msu.edu>
2024-03-18 18:25:28 -0400

    new changes

---
+++ importbooks.js
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://teymsckkkueaiccnmbma.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRleW1zY2tra3VlYWljY25tYm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTg5NDUsImV4cCI6MjAyNjM3NDk0NX0.9b3sxKHSVSztgVP7t7FvX1WGZL-jubdRz_QwQ7JPCZQ'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+let { data: Five_Books, error } = await supabase
+  .from('Five_Books')
+  .select('*')
+
+for (let My_Books of Five_Books) {
+    let mybookTable = document.getElementById('Five_Books');
+    mybookTable.innerHTML = '<table>${My_Books.Title}</table>';
+}
