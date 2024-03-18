3608052446a97bb635d43588e93470a4866a1961
Randi Gabbard <gabbardr@msu.edu>
2024-03-18 18:48:47 -0400

    changes

--- importbooks.js
+++ importbooks.js
@@ -9,5 +9,5 @@ let { data: Five_Books, error } = await supabase
 
 for (let My_Books of Five_Books) {
     let mybookTable = document.getElementById('Five_Books');
-    mybookTable.innerHTML += '<ul>${My_Books.Title} ${My_Books.Author}</ul>';
+    mybookTable.innerHTML += '<ul>${My_Books.Title}</ul>';
 }
