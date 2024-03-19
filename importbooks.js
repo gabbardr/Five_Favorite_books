542b935b74e13e008cba0a9c18e247268230d4e2
Randi Gabbard <gabbardr@msu.edu>
2024-03-18 22:51:06 -0400

    changes

--- importbooks.js
+++ importbooks.js
@@ -1,15 +1,19 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://teymsckkkueaiccnmbma.supabase.co/rest/v1/Five_Books'
+const supabaseUrl = 'https://teymsckkkueaiccnmbma.supabase.co'
 const supabaseKey = 
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRleW1zY2tra3VlYWljY25tYm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTg5NDUsImV4cCI6MjAyNjM3NDk0NX0.9b3sxKHSVSztgVP7t7FvX1WGZL-jubdRz_QwQ7JPCZQ'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-let { data: Five_Books, error } = await supabase
-  .from('Five_Books')
-  .select('*')
-
-for (let book of Five_Books) {
-    let mybookTable = document.getElementById('Five_Books');
-    mybookTable.innerHTML += `<li>${book.Title}</li>`;
+async function pullBook() {
+    let { data: FiveBooks, error } = await supabase
+    .from('FiveBooks')
+    .select('*')
+  
+  for (let book of FiveBooks) {
+      let mybookTable = document.getElementById('Five_Books');
+      mybookTable.innerHTML += `<li>${book.Title}</li>`;
+  }
 }
+
+pullBook();
