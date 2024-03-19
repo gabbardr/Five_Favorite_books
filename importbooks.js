cdd0cd6ac7d439a72c72eaaf417b32f9b285b296
Randi Gabbard <gabbardr@msu.edu>
2024-03-18 22:26:48 -0400

    changes

--- importbooks.js
+++ importbooks.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://teymsckkkueaiccnmbma.supabase.co'
+const supabaseUrl = 'https://teymsckkkueaiccnmbma.supabase.co/rest/v1/Five_Books'
 const supabaseKey = 
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRleW1zY2tra3VlYWljY25tYm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTg5NDUsImV4cCI6MjAyNjM3NDk0NX0.9b3sxKHSVSztgVP7t7FvX1WGZL-jubdRz_QwQ7JPCZQ'
 const supabase = createClient(supabaseUrl, supabaseKey)
